e4822c06b494802e219964340e454fdcb803fd25
Masoud Khodadadi <masoud.xpress@gmail.com>
2020-04-01 20:54:16 +0430

    Add Doctor list and pagination and login register panel

--- Clinic.WebApplication/Controllers/HomeController.cs
+++ Clinic.WebApplication/Controllers/HomeController.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Clinic.DataContext;
+using Clinic.Models.DomainClasses.Users;
+using Clinic.Utilities.Pagination;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Clinic.WebApplication.Controllers
 {
@@ -22,6 +25,42 @@ namespace Clinic.WebApplication.Controllers
             //TODO:Add RoleChecking
 
             return View(news);
+        }
+
+        public async Task<IActionResult> DoctorList(string searchString, string currentFilter, int? page)
+        {
+            //TODO: Add RoleChecking
+            
+            if (searchString != null)
+            {
+                page = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+
+            ViewBag.CurrentFilter = searchString;
+
+            var doctors = _db.Doctors;
+
+            int pageSize = 9;
+            int pageNumber = (page ?? 1);
+
+            if (string.IsNullOrEmpty(searchString))
+            {
+                return View(await PaginatedList<Doctor>.CreateAsync(doctors.AsNoTracking(), pageNumber, pageSize));
+            }
+
+            var searchResults =
+                _db.Doctors.Where(a => a.FullName.Contains(searchString) || a.Specialty.Contains(searchString));
+
+            return View(await PaginatedList<Doctor>.CreateAsync(searchResults.AsNoTracking(), pageNumber, pageSize));
+        }
+
+        public IActionResult LoginRegisterPanel()
+        {
+            return View();
         }
     }
 }
