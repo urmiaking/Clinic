34f74dd68b717c0f716a31ecba12d7b96b731c18
Masoud Khodadadi <masoud.xpress@gmail.com>
2020-04-01 20:07:51 +0430

    Adding Index page for home controller

--- Clinic.WebApplication/Controllers/HomeController.cs
+++ Clinic.WebApplication/Controllers/HomeController.cs
@@ -2,15 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Clinic.DataContext;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Clinic.WebApplication.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly AppDbContext _db;
+
+        public HomeController(AppDbContext db)
+        {
+            this._db = db;
+        }
         public IActionResult Index()
         {
-            return View();
+            var news = _db.News.OrderByDescending(n => n.ReleaseDate).Take(6).ToList();
+
+            //TODO:Add RoleChecking
+
+            return View(news);
         }
     }
 }
