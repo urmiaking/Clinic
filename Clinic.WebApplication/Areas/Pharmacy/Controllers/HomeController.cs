7f27d3fc32d0cf1788d2e26de798de0dcf655422
Masoud Khodadadi <masoud.xpress@gmail.com>
2020-04-04 05:01:59 +0430

    Implement Index Page pharmacy panel

--- Clinic.WebApplication/Areas/Pharmacy/Controllers/HomeController.cs
+++ Clinic.WebApplication/Areas/Pharmacy/Controllers/HomeController.cs
@@ -2,8 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Clinic.DataContext;
+using Clinic.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Clinic.WebApplication.Areas.Pharmacy.Controllers
 {
@@ -11,9 +14,37 @@ namespace Clinic.WebApplication.Areas.Pharmacy.Controllers
     [Authorize(Roles = "Pharmacy")]
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly AppDbContext _db;
+        public HomeController(AppDbContext db)
         {
-            return View();
+            _db = db;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var drugs = await _db.Drugs.ToListAsync();
+
+            var prescriptions = await _db.Prescriptions
+                    .Include(a => a.PrescriptionDrugs)
+                    .Include(a => a.Visit)
+                        .ThenInclude(a => a.Reservation)
+                            .ThenInclude(a => a.Patient)
+                    .Include(a => a.Visit)
+                        .ThenInclude(a => a.InsuranceProvider)
+                    .Where(a =>
+                        a.Status.Equals("پرداخت نشده") ||
+                        a.PaymentMethod.Equals("غیر نقدی")).ToListAsync();
+
+            ViewBag.AllPrescriptions = await _db.Prescriptions.CountAsync();
+            ViewBag.AllDeliveredPrescriptions = await _db.Prescriptions.CountAsync(a => a.Status.Equals("تحویل داده شده"));
+
+            var prescriptionDrugVm = new PharmacyDrugPrescriptionViewModel()
+            {
+                Drugs = drugs,
+                Prescriptions = prescriptions
+            };
+
+            return View(prescriptionDrugVm);
         }
     }
 }
