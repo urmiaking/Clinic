1bbadcd2c609788eef911ef1affd5d4ec6b9cda0
Masoud Khodadadi <masoud.xpress@gmail.com>
2020-04-05 23:53:20 +0430

    implement patient index page

--- Clinic.WebApplication/Areas/Patient/Controllers/HomeController.cs
+++ Clinic.WebApplication/Areas/Patient/Controllers/HomeController.cs
@@ -1,9 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Clinic.DataContext;
+using Clinic.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Clinic.WebApplication.Areas.Patient.Controllers
 {
@@ -11,9 +13,43 @@ namespace Clinic.WebApplication.Areas.Patient.Controllers
     [Authorize(Roles = "Patient")]
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly AppDbContext _db;
+
+        public HomeController(AppDbContext db)
         {
-            return View();
+            _db = db;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var patient = await _db.Patients.FirstOrDefaultAsync(a => a.Username.Equals(User.Identity.Name));
+
+            var visits = await _db.Visits
+                .Include(a => a.Reservation)
+                    .ThenInclude(a => a.Doctor)
+                .Where(a => a.Reservation.PatientId.Equals(patient.Id)).ToListAsync();
+
+            ViewBag.TotalVisitsCount = visits.Count;
+            ViewBag.TotalReservesCount = await _db.Reservations
+                .Where(a =>
+                    a.PatientId.Equals(patient.Id) && a.ReserveStatus.Contains("در انتظار ویزیت"))
+                .CountAsync();
+
+            ViewBag.MonthlyVisitsCount = visits.Count(visit => (visit.Reservation.ReserveDate - DateTime.Now).TotalDays < 30);
+
+            var reserves = await _db.Reservations.Include(a => a.Doctor)
+                .Where(a => 
+                    a.PatientId.Equals(patient.Id) && 
+                    a.ReserveDate.Day.Equals(DateTime.Now.Day) && 
+                    a.ReserveStatus.Equals("در انتظار ویزیت"))
+                .ToListAsync();
+
+            var reserveVisitViewModel = new ReserveVisitViewModel()
+            {
+                Reserves = reserves,
+                Visits = visits
+            };
+
+            return View(reserveVisitViewModel);
         }
     }
 }
