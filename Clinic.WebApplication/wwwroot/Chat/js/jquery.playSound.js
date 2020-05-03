b8600fc7f894abc10117acedbdc322037fd35a48
Masoud Khodadadi <masoud.xpress@gmail.com>
2020-05-03 06:13:48 +0430

    remove unneccesary js file

--- Clinic.WebApplication/wwwroot/Chat/js/jquery.playSound.js
+++ Clinic.WebApplication/wwwroot/Chat/js/jquery.playSound.js
@@ -7,20 +7,24 @@
  * $.playSound('/attachments/sounds/1234.wav')
  * $.playSound('/attachments/sounds/1234.mp3')
  * $.stopSound();
-**/
+ **/
 
 (function ($) {
-    $.extend({
-        playSound: function () {
-            return $(
-                   '<audio class="sound-player" autoplay="autoplay" style="display:none;">'
-                     + '<source src="' + arguments[0] + '" />'
-                     + '<embed src="' + arguments[0] + '" hidden="true" autostart="true" loop="false"/>'
-                   + '</audio>'
-                 ).appendTo('body');
-        },
-        stopSound: function () {
-            $(".sound-player").remove();
-        }
-    });
+  $.extend({
+    playSound: function () {
+      return $(
+        '<audio class="sound-player" autoplay="autoplay" style="display:none;">' +
+          '<source src="' +
+          arguments[0] +
+          '" />' +
+          '<embed src="' +
+          arguments[0] +
+          '" hidden="true" autostart="true" loop="false"/>' +
+          "</audio>"
+      ).appendTo("body");
+    },
+    stopSound: function () {
+      $(".sound-player").remove();
+    },
+  });
 })(jQuery);
